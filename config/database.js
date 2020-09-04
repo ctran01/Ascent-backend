30b5e62e8082ff8f5e765360b3304b1bf556c7bc
ctran01 <chris@DESKTOP-6RFRI4D.localdomain>
2020-09-04 12:30:19 -0500

    troubleshooting

--- config/database.js
+++ config/database.js
@@ -5,8 +5,7 @@ const username = db.username;
 const password = db.password;
 const database = db.database;
 const host = db.host;
-const jwt = config.jwt;
-const secret = jwt.secret
+
 
 module.exports = {
   development: {
@@ -19,6 +18,6 @@ module.exports = {
   production:{
     dialect: "postgres",
     use_env_variable: "DATABASE_URL",
-    secret
+    
   }
 };
